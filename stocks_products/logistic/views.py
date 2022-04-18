0eb087ffb5f02e9e96864312ce10428f97bebcf0
OksanaSha <oksanashatalova@bk.ru>
2022-04-18 23:56:04 +0300

    Initial commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,31 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from django_filters import rest_framework
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = PageNumberPagination
+
+# class UserFilter(SearchFilter):
+#     search_param = 'products'
+#
+#     def get_search_fields(self, view, request):
+#         return ['products__title', 'products__description', 'products']
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend,]
+    filterset_fields = ['products__title']
+    # filterset_class = UserFilter
+    pagination_class = PageNumberPagination
+
